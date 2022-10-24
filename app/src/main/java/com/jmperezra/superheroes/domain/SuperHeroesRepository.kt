2625aec9a3d9a15eb664f7c694f10e2063c31947
Chema Pramos <jmperezra@gmail.com>
2022-10-24 19:28:31 +0200

    created local source

--- app/src/main/java/com/jmperezra/superheroes/domain/SuperHeroesRepository.kt
+++ app/src/main/java/com/jmperezra/superheroes/domain/SuperHeroesRepository.kt
@@ -3,4 +3,5 @@ package com.jmperezra.superheroes.domain
 interface SuperHeroesRepository {
     fun saveSuperHeroes(superHeroes: List<SuperHeroe>)
     fun getSuperHeroes(): List<SuperHeroe>
+    fun getSuperHeroe(superHeroeId: Int): SuperHeroe?
 }
