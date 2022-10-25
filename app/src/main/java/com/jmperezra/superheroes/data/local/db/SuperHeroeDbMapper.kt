a5f9ed99b1c10d2a2b42f7154e9be218c2e585ee
Chema Pramos <jmperezra@gmail.com>
2022-10-25 20:13:48 +0200

    update database

--- app/src/main/java/com/jmperezra/superheroes/data/local/db/SuperHeroeDbMapper.kt
+++ app/src/main/java/com/jmperezra/superheroes/data/local/db/SuperHeroeDbMapper.kt
@@ -3,3 +3,4 @@ package com.jmperezra.superheroes.data.local.db
 import com.jmperezra.superheroes.domain.SuperHeroe
 
 fun SuperHeroeEntity.toDomain() = SuperHeroe(this.id, this.name, this.imageUrl)
+fun SuperHeroe.toEntity() = SuperHeroeEntity(this.id, this.name, this.imageUrl)
